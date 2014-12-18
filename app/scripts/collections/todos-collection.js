3fb5892478d535a3200a1f9cae5d6a38eecf16c8
fanyong <y.fan@Ctrip.com>
2014-12-18 18:06:33 +0800

    add collection

---
+++ app/scripts/collections/todos-collection.js
@@ -0,0 +1,14 @@
+
+(function(){
+
+    'use strict';
+
+    backboneApp.Collections.TodosCollection = Backbone.Collection.extend({
+
+        localStorage: new Backbone.LocalStorage('backbone-generator-todos'),
+
+        initialize: function(){
+            this.model = backboneApp.Models.TodoModel;
+        }
+    });
+}();
---
+++ app/scripts/views/todos-view.js
@@ -0,0 +1,58 @@
+/*global backboneTodos, Backbone, JST*/
+
+(function () {
+    'use strict';
+
+    backboneApp.Views.TodosView = Backbone.View.extend({
+
+        el: '#todo-app',
+
+        template: JST['app/scripts/templates/todos.ejs'],
+
+        events: {
+            'submit': 'createTodo'
+        },
+
+        initialize: function () {
+            this.render();
+
+            this.listenTo(this.model, 'add', this.addTodoItem);
+            this.listenTo(this.model, 'reset', this.addAllTodoItems);
+
+            this.collection.fetch();
+        },
+
+        render: function () {
+            this.$el.html(this.template());
+
+            return this;
+        },
+
+        createTodo: function(event){
+            event.preventDefault();
+
+            var title = this.$("#new-todo").val().trim();
+
+            if (title) {
+                this.collection.create(new backboneApp.Models.TodoModel({
+                    title: title
+                }));
+            };
+
+            $("#new-todo").val('');
+        },
+
+        addTodoItem: function(){
+            var view = new backboneApp.Views.TodosView({
+                model: todo
+            });
+            this.$('ul').append(view.render().el);
+        },
+
+        addAllTodoItems: function(){
+            this.collection.each(this.addTodoItem, this);
+        }
+
+    });
+
+})();

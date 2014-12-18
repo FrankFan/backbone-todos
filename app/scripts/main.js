6a8afb10b279f1ee11aef3c806107184fdcfb353
fanyong <y.fan@Ctrip.com>
2014-12-18 17:18:14 +0800

    finish main.js and index.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
 console.log('\'Allo \'Allo!');
+
+/*global backboneApp */
+
+(function(){
+
+    'use strict';
+
+    window.backboneApp = {
+        Models: {},
+        Collections: {},
+        Views: {},
+        Routers: {},
+        init: function(){
+            new this.Views.TodosView({
+                collection: new this.Collections.TodosCollection()
+            });
+        }
+    };
+
+    $(document).ready(function(){
+        backboneApp.init();
+    });
+
+})();

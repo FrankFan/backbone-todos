7adc24ba487ab5853acfd742e1cff7849639d790
fanyong557 <fanyong557@pingan.com.cn>
2015-06-23 13:57:38 +0800

    增加js

--- simple-version/README.md
+++ simple-version/README.md
@@ -1,5 +1,4 @@
 最简单的todo app
 
 
-
 [参考](http://backbonejs.org/examples/todos/index.html)
--- simple-version/todos.js
+++ simple-version/todos.js
@@ -1,1 +1,26 @@
-todos.js
+/*
+ * @description: todo js
+ * @refer: http://backbonejs.org/docs/todos.html
+ */
+
+$(function(){
+
+	// TODO model
+
+	var Todo = Backbone.Model.extend({
+		defaults: function() {
+			return {
+				title: 'empty todo...',
+				order: Todos.nextOrder(),
+				done: false
+			};
+		},
+
+		toggle: function() {
+			this.save({
+				done: !this.get('done')
+			});
+		}
+	});
+	
+});

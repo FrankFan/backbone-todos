632d578b291bf8c23a4397b0969adf0921965461
fanyong557 <fanyong557@pingan.com.cn>
2015-06-25 20:49:03 +0800

    增加app view 的js 逻辑

--- simple-version/todos.js
+++ simple-version/todos.js
@@ -121,18 +121,45 @@ $(function(){
 
 		initialize: function() {
 
+			this.input = this.$('#new-todo');
+			this.allCheckbox = this.$('#toggle-all')[0];
+
+			this.footer = this.$('footer');
+			this.main = $('#main');
+
+			this.listenTo(Todos, 'add', this.addOne);
+			this.listenTo(Todos, 'reset', this.addAll);
+			this.listenTo(Todos, 'all', this.render);
+
+			Todos.fetch();
 		},
 
 		render: function() {
+			var done = Todos.done().length;
+			var remaining = Todos.remaining().length;
 
+			if(Todos.length) {
+				this.main.show();
+				this.footer.show();
+				this.footer.html(this.statsTemplate({
+					done: done,
+					remaining: remaining
+				}));
+			} else {
+				this.main.hide();
+				this.footer.hide();
+			}
+
+			this.allCheckbox.checked = !remaining;
 		},
 
-		addOne: function() {
-
+		addOne: function(todo) {
+			var view = new TodoView({ model: todo });
+			this.$('#todo-list').append(view.render().el);
 		},
 
 		addAll: function() {
-
+			Todos.each(this.addOne, this);
 		},
 
 		createOnEnter: function(e) {
